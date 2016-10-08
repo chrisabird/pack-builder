921dcfaaf78dd56d2a4f1899371160337b3819e1
chris <chris@christopherbird.co.uk>
2016-10-08 22:08:26 +0100

    Set verison to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pack-builder "0.1.0-SNAPSHOT"
+(defproject pack-builder "0.1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [reagent "0.5.1"]
